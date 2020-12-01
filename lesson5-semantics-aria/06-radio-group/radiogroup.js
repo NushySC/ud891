f5e65f6d9f662b41f03713967a1e8a94829e1532
Anuska Sampedro <asampedro@vectoritcgroup.com>
2020-12-01 20:33:07 +0100

    aria checked

--- lesson5-semantics-aria/06-radio-group/radiogroup.js
+++ lesson5-semantics-aria/06-radio-group/radiogroup.js
@@ -1,108 +1,101 @@
-(function() {
-  'use strict';
+(function () {
+	'use strict';
 
-  // Define values for keycodes
-  var VK_ENTER      = 13;
-  var VK_SPACE      = 32;
-  var VK_LEFT       = 37;
-  var VK_UP         = 38;
-  var VK_RIGHT      = 39;
-  var VK_DOWN       = 40;
+	// Define values for keycodes
+	var VK_ENTER = 13;
+	var VK_SPACE = 32;
+	var VK_LEFT = 37;
+	var VK_UP = 38;
+	var VK_RIGHT = 39;
+	var VK_DOWN = 40;
 
-  // Helper function to convert NodeLists to Arrays
-  function slice(nodes) {
-    return Array.prototype.slice.call(nodes);
-  }
+	// Helper function to convert NodeLists to Arrays
+	function slice(nodes) {
+		return Array.prototype.slice.call(nodes);
+	}
 
-  function RadioGroup(id) {
-    this.el = document.querySelector(id);
-    this.buttons = slice(this.el.querySelectorAll('.radio'));
-    this.focusedIdx = 0;
-    this.focusedButton = this.buttons[this.focusedIdx];
+	function RadioGroup(id) {
+		this.el = document.querySelector(id);
+		this.buttons = slice(this.el.querySelectorAll('.radio'));
+		this.focusedIdx = 0;
+		this.focusedButton = this.buttons[this.focusedIdx];
 
-    this.el.addEventListener('keydown', this.handleKeyDown.bind(this));
-    this.el.addEventListener('click', this.handleClick.bind(this));
+		this.el.addEventListener('keydown', this.handleKeyDown.bind(this));
+		this.el.addEventListener('click', this.handleClick.bind(this));
 
-    // Any more initialization to do here?
+		// Set ARIA role for the radio group.
+		this.el.setAttribute('role', 'radiogroup');
 
-    var firstButton = true;
-    for (var button of this.buttons) {
-      if (firstButton) {
-        button.tabIndex = "0";
-        firstButton = false;
-      } else {
-        button.tabIndex = "-1";
-      }
+		var firstButton = true;
+		for (var button of this.buttons) {
+			if (firstButton) {
+				button.tabIndex = '0';
+				firstButton = false;
+			} else {
+				button.tabIndex = '-1';
+			}
 
-      // What about here?
-    }
+			button.setAttribute('role', 'radio');
+		}
+	}
 
-  }
+	RadioGroup.prototype.handleKeyDown = function (e) {
+		switch (e.keyCode) {
+			case VK_UP:
+			case VK_LEFT: {
+				e.preventDefault();
 
-  RadioGroup.prototype.handleKeyDown = function(e) {
-    switch(e.keyCode) {
+				this.focusedIdx--;
+				if (this.focusedIdx < 0)
+					this.focusedIdx = this.focusedIdx + this.buttons.length;
 
-      case VK_UP:
-      case VK_LEFT: {
+				break;
+			}
 
-        e.preventDefault();
+			case VK_DOWN:
+			case VK_RIGHT: {
+				e.preventDefault();
 
-        this.focusedIdx--;
-        if (this.focusedIdx < 0)
-          this.focusedIdx = this.focusedIdx + this.buttons.length;
+				this.focusedIdx = (this.focusedIdx + 1) % this.buttons.length;
 
-        break;
-      }
+				break;
+			}
 
-      case VK_DOWN:
-      case VK_RIGHT: {
+			case VK_SPACE:
+				var focusedButton = e.target;
+				var idx = this.buttons.indexOf(focusedButton);
+				if (idx < 0) return;
+				this.focusedIdx = idx;
+				break;
 
-        e.preventDefault();
+			default:
+				return;
+		}
 
-        this.focusedIdx = (this.focusedIdx + 1) % this.buttons.length;
+		this.changeFocus();
+	};
 
-        break;
-      }
+	RadioGroup.prototype.handleClick = function (e) {
+		var button = e.target;
+		var idx = this.buttons.indexOf(button);
+		if (idx < 0) return;
+		this.focusedIdx = idx;
+		this.changeFocus();
+	};
 
-    case VK_SPACE:
-        var focusedButton = e.target;
-        var idx = this.buttons.indexOf(focusedButton);
-        if (idx < 0)
-          return;
-        this.focusedIdx = idx;
-        break;
+	RadioGroup.prototype.changeFocus = function () {
+		// Set the old button to tabindex -1
+		this.focusedButton.tabIndex = -1;
+		this.focusedButton.removeAttribute('checked');
+		this.focusedButton.setAttribute('aria-checked', 'false');
 
-      default:
-        return;
-    }
+		// Set the new button to tabindex 0 and focus it
+		this.focusedButton = this.buttons[this.focusedIdx];
+		this.focusedButton.tabIndex = 0;
+		this.focusedButton.focus();
+		this.focusedButton.setAttribute('checked', '');
+		this.focusedButton.setAttribute('aria-checked', 'true');
+	};
 
-    this.changeFocus();
-  };
-
-  RadioGroup.prototype.handleClick = function(e) {
-    var button = e.target;
-    var idx = this.buttons.indexOf(button);
-    if (idx < 0)
-      return;
-    this.focusedIdx = idx;
-    this.changeFocus();
-  };
-
-  RadioGroup.prototype.changeFocus = function() {
-    // Set the old button to tabindex -1
-    this.focusedButton.tabIndex = -1;
-    this.focusedButton.removeAttribute('checked');
-
-    // Set the new button to tabindex 0 and focus it
-    this.focusedButton = this.buttons[this.focusedIdx];
-    this.focusedButton.tabIndex = 0;
-    this.focusedButton.focus();
-    this.focusedButton.setAttribute('checked', '');
-
-    // ... we probably want to do some stuff here, too ...
-
-  };
-
-  var group1 = new RadioGroup('#group1');
-
-}());
+	var group1 = new RadioGroup('#group1');
+})();
